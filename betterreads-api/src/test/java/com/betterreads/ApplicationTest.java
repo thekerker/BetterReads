8ad484d1e447e34f36c2f842042c7196b2867478
Nicholas <nick.kerker@gmail.com>
2023-09-02 17:12:01 -0500

    see #3 initial project creation

---
+++ betterreads-api/src/test/java/com/betterreads/ApplicationTest.java
@@ -0,0 +1,18 @@
+package com.betterreads;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ApplicationTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}

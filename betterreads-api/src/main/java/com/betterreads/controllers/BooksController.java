2aa9f3b31c7ed6621845915a33dc7fbdb03d6192
Nicholas <nick.kerker@gmail.com>
2023-09-12 17:47:54 -0500

    see #12 added better swagger information to the controllers

--- betterreads-api/src/main/java/com/betterreads/controllers/BooksController.java
+++ betterreads-api/src/main/java/com/betterreads/controllers/BooksController.java
@@ -19,6 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.betterreads.models.Book;
 import com.betterreads.services.IService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -41,6 +46,11 @@ public class BooksController extends BaseController {
      * 
      * @return all books
      */
+    @Operation(summary = "Gets all books from the data store")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found books", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Book.class)) })
+    })
     @GetMapping(path = "/books")
     public CollectionModel<EntityModel<?>> getAll() {
 
@@ -57,6 +67,14 @@ public class BooksController extends BaseController {
      * @param id id of the book to retrieve
      * @return the book
      */
+    @Operation(summary = "Gets a book from the data store by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the book", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Book.class)) }),
+            @ApiResponse(responseCode = "404", description = "No book found for the id", content = {
+                    @Content
+            })
+    })
     @GetMapping(path = "/books/{id}")
     public ResponseEntity<?> getById(@PathVariable("id") String id) {
         EntityModel<?> entity = booksService.getById(id);
@@ -76,6 +94,11 @@ public class BooksController extends BaseController {
      * @param request the search request parameters
      * @return all books that match the search criteria
      */
+    @Operation(summary = "Searches for books based on a set of criteria")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found results", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Book.class)) })
+    })
     @PostMapping(path = "/books/search")
     public CollectionModel<EntityModel<?>> search(@RequestBody Book request) {
         List<EntityModel<?>> books = booksService.search(request);
@@ -91,6 +114,11 @@ public class BooksController extends BaseController {
      * @param book the book to save
      * @return the book that was saved
      */
+    @Operation(summary = "Adds a new book to the data store")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Added the book", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Book.class)) })
+    })
     @PostMapping(path = "/books")
     public ResponseEntity<?> add(@Valid @RequestBody Book book) {
         EntityModel<?> entity = booksService.add(book);
@@ -107,6 +135,11 @@ public class BooksController extends BaseController {
      * @param book the book to update
      * @return the updated book
      */
+    @Operation(summary = "Updates a book in the data store by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Updated the book", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Book.class)) })
+    })
     @PutMapping(path = "/books/{id}")
     public ResponseEntity<?> update(@PathVariable("id") String id, @Valid @RequestBody Book book) {
         EntityModel<?> entity = booksService.update(id, book);
@@ -121,6 +154,11 @@ public class BooksController extends BaseController {
      * 
      * @param id the id of the book to delete
      */
+    @Operation(summary = "Deletes a book from the data store by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Deleted the book", content = {
+                    @Content })
+    })
     @DeleteMapping(path = "/books/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") String id) {
         booksService.delete(id);
@@ -133,6 +171,11 @@ public class BooksController extends BaseController {
      * Deletes all books in the repository
      * </p>
      */
+    @Operation(summary = "Deletes all books from the data store")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Deleted all books", content = {
+                    @Content })
+    })
     @DeleteMapping(path = "/books")
     public ResponseEntity<?> deleteAll() {
         booksService.deleteAll();
--- betterreads-api/src/main/java/com/betterreads/controllers/PublishersController.java
+++ betterreads-api/src/main/java/com/betterreads/controllers/PublishersController.java
@@ -19,6 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.betterreads.models.Publisher;
 import com.betterreads.services.IService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -31,113 +36,152 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RequestMapping("/v1")
 public class PublishersController extends BaseController {
 
-    @Autowired
-    private IService publishersService;
+        @Autowired
+        private IService publishersService;
 
-    /**
-     * <p>
-     * Gets publishers from the data store
-     * </p>
-     * 
-     * @return all publishers
-     */
-    @GetMapping(path = "/publishers")
-    public CollectionModel<EntityModel<?>> getAll() {
+        /**
+         * <p>
+         * Gets publishers from the data store
+         * </p>
+         * 
+         * @return all publishers
+         */
+        @Operation(summary = "Gets all publishers from the data store")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "Found publishers", content = {
+                                        @Content(mediaType = "application/json", schema = @Schema(implementation = Publisher.class)) })
+        })
+        @GetMapping(path = "/publishers")
+        public CollectionModel<EntityModel<?>> getAll() {
 
-        List<EntityModel<?>> publishers = publishersService.getAll();
+                List<EntityModel<?>> publishers = publishersService.getAll();
 
-        return CollectionModel.of(publishers, linkTo(methodOn(PublishersController.class).getAll()).withSelfRel());
-    }
-
-    /**
-     * <p>
-     * Gets publishers by id
-     * <p>
-     * 
-     * @param id id of the publisher to retrieve
-     * @return the publisher
-     */
-    @GetMapping(path = "/publishers/{id}")
-    public ResponseEntity<?> getById(@PathVariable("id") String id) {
-        EntityModel<?> entity = publishersService.getById(id);
-
-        if (entity == null) {
-            return ResponseEntity.notFound().build();
+                return CollectionModel.of(publishers,
+                                linkTo(methodOn(PublishersController.class).getAll()).withSelfRel());
         }
 
-        return ResponseEntity.ok(entity);
-    }
+        /**
+         * <p>
+         * Gets publishers by id
+         * <p>
+         * 
+         * @param id id of the publisher to retrieve
+         * @return the publisher
+         */
+        @Operation(summary = "Gets a publisher from the data store by its id")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "Found the publisher", content = {
+                                        @Content(mediaType = "application/json", schema = @Schema(implementation = Publisher.class)) }),
+                        @ApiResponse(responseCode = "404", description = "No publisher found for the id", content = {
+                                        @Content
+                        })
+        })
+        @GetMapping(path = "/publishers/{id}")
+        public ResponseEntity<?> getById(@PathVariable("id") String id) {
+                EntityModel<?> entity = publishersService.getById(id);
 
-    /**
-     * <p>
-     * Searches for all publishers based on the provided criteria
-     * </p>
-     * 
-     * @param request the search request parameters
-     * @return all publishers that match the search criteria
-     */
-    @PostMapping(path = "/publishers/search")
-    public CollectionModel<EntityModel<?>> search(@RequestBody Publisher request) {
-        List<EntityModel<?>> publishers = publishersService.search(request);
+                if (entity == null) {
+                        return ResponseEntity.notFound().build();
+                }
 
-        return CollectionModel.of(publishers,
-                linkTo(methodOn(PublishersController.class).search(request)).withSelfRel());
-    }
+                return ResponseEntity.ok(entity);
+        }
 
-    /**
-     * <p>
-     * Saves a publisher in the repository
-     * <p>
-     * 
-     * @param publisher the publisher to save
-     * @return the publisher that was saved
-     */
-    @PostMapping(path = "/publishers")
-    public ResponseEntity<?> add(@Valid @RequestBody Publisher publisher) {
-        EntityModel<?> entity = publishersService.add(publisher);
+        /**
+         * <p>
+         * Searches for all publishers based on the provided criteria
+         * </p>
+         * 
+         * @param request the search request parameters
+         * @return all publishers that match the search criteria
+         */
+        @Operation(summary = "Searches for publishers based on a set of criteria")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "Found results", content = {
+                                        @Content(mediaType = "application/json", schema = @Schema(implementation = Publisher.class)) })
+        })
+        @PostMapping(path = "/publishers/search")
+        public CollectionModel<EntityModel<?>> search(@RequestBody Publisher request) {
+                List<EntityModel<?>> publishers = publishersService.search(request);
 
-        return ResponseEntity.created(entity.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entity);
-    }
+                return CollectionModel.of(publishers,
+                                linkTo(methodOn(PublishersController.class).search(request)).withSelfRel());
+        }
 
-    /**
-     * <p>
-     * Updates a publisher in the repository by the provided id
-     * </p>
-     * 
-     * @param id        the id of the publisher
-     * @param publisher the publisher to update
-     * @return the updated publisher
-     */
-    @PutMapping(path = "/publishers/{id}")
-    public ResponseEntity<?> update(@PathVariable("id") String id, @Valid @RequestBody Publisher publisher) {
-        EntityModel<?> entity = publishersService.update(id, publisher);
+        /**
+         * <p>
+         * Saves a publisher in the repository
+         * <p>
+         * 
+         * @param publisher the publisher to save
+         * @return the publisher that was saved
+         */
+        @Operation(summary = "Adds a new publisher to the data store")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "201", description = "Added the publisher", content = {
+                                        @Content(mediaType = "application/json", schema = @Schema(implementation = Publisher.class)) })
+        })
+        @PostMapping(path = "/publishers")
+        public ResponseEntity<?> add(@Valid @RequestBody Publisher publisher) {
+                EntityModel<?> entity = publishersService.add(publisher);
 
-        return ResponseEntity.created(entity.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entity);
-    }
+                return ResponseEntity.created(entity.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entity);
+        }
 
-    /**
-     * <p>
-     * Deletes the publisher associated with the provided id
-     * </p>
-     * 
-     * @param id the id of the publisher to delete
-     */
-    @DeleteMapping(path = "/publishers/{id}")
-    public ResponseEntity<?> delete(@PathVariable("id") String id) {
-        publishersService.delete(id);
+        /**
+         * <p>
+         * Updates a publisher in the repository by the provided id
+         * </p>
+         * 
+         * @param id        the id of the publisher
+         * @param publisher the publisher to update
+         * @return the updated publisher
+         */
+        @Operation(summary = "Updates a publisher in the data store by its id")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "201", description = "Updated the publisher", content = {
+                                        @Content(mediaType = "application/json", schema = @Schema(implementation = Publisher.class)) })
+        })
+        @PutMapping(path = "/publishers/{id}")
+        public ResponseEntity<?> update(@PathVariable("id") String id, @Valid @RequestBody Publisher publisher) {
+                EntityModel<?> entity = publishersService.update(id, publisher);
 
-        return ResponseEntity.noContent().build();
-    }
+                return ResponseEntity.created(entity.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entity);
+        }
 
-    /**
-     * <p>
-     * Deletes all publishers in the repository
-     * </p>
-     */
-    @DeleteMapping(path = "/publishers")
-    public ResponseEntity<?> deleteAll() {
-        publishersService.deleteAll();
+        /**
+         * <p>
+         * Deletes the publisher associated with the provided id
+         * </p>
+         * 
+         * @param id the id of the publisher to delete
+         */
+        @Operation(summary = "Deletes a publisher from the data store by its id")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "204", description = "Deleted the publisher", content = {
+                                        @Content })
+        })
+        @DeleteMapping(path = "/publishers/{id}")
+        public ResponseEntity<?> delete(@PathVariable("id") String id) {
+                publishersService.delete(id);
 
-        return ResponseEntity.noContent().build();
-    }
+                return ResponseEntity.noContent().build();
+        }
+
+        /**
+         * <p>
+         * Deletes all publishers in the repository
+         * </p>
+         */
+        @Operation(summary = "Deletes all publishers from the data store")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "204", description = "Deleted all publishers", content = {
+                                        @Content })
+        })
+        @DeleteMapping(path = "/publishers")
+        public ResponseEntity<?> deleteAll() {
+                publishersService.deleteAll();
+
+                return ResponseEntity.noContent().build();
+        }
 }

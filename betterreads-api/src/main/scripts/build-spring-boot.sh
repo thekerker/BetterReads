f1c4bfaecab1fb20dd8b95b2b7ae4ad60df26155
Nicholas <nick.kerker@gmail.com>
2023-09-12 22:11:13 -0500

    see #13 restored dynamic image name

--- betterreads-api/src/main/scripts/build-spring-boot.sh
+++ betterreads-api/src/main/scripts/build-spring-boot.sh
@@ -5,8 +5,8 @@ docker login -u="$USERNAME" -p="$PASSWORD"
 mvn spring-boot:build-image \
     --batch-mode --no-transfer-progress
 
-#IMAGE_NAME=$(mvn help:evaluate -Dexpression=docker.image.name -q -DforceStdout)
-IMAGE_NAME="docker.io/library/betterreads-api:1.0-SNAPSHOT"
+IMAGE_NAME=$(mvn help:evaluate -Dexpression=module.image.name -q -DforceStdout)
+#IMAGE_NAME="docker.io/library/betterreads-api:1.0-SNAPSHOT"
 echo $IMAGE_NAME
 
 docker push $IMAGE_NAME

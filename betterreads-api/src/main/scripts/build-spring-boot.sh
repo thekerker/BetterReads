8b34da121fa923460c8495ba69b461206e5324f9
Nicholas <nick.kerker@gmail.com>
2023-09-12 21:59:41 -0500

    see #13 hard coding image name for now

--- betterreads-api/src/main/scripts/build-spring-boot.sh
+++ betterreads-api/src/main/scripts/build-spring-boot.sh
@@ -5,7 +5,8 @@ docker login "https://index.docker.io/v1/" -u="$USERNAME" -p="$PASSWORD"
 mvn spring-boot:build-image \
     --batch-mode --no-transfer-progress
 
-IMAGE_NAME=$(mvn help:evaluate -Dexpression=docker.image.name -q -DforceStdout)
+#IMAGE_NAME=$(mvn help:evaluate -Dexpression=docker.image.name -q -DforceStdout)
+IMAGE_NAME="docker.io/library/betterreads-api:1.0-SNAPSHOT"
 echo $IMAGE_NAME
 
 docker push $IMAGE_NAME
